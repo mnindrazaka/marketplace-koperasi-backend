4ae7ac9808afb71bffebfe8e1d258ae288309ad2
mnindrazaka <mnindrazaka@gmail.com>
2018-05-01 05:31:18 +0700

    impelement validation on modelbase using JOI

--- controller/kategori.js
+++ controller/kategori.js
@@ -2,13 +2,6 @@ const kategori = require('../model/kategori')
 
 const controller = {
   index: async (req, res) => {
-    // kategori.fetchAll()
-    //   .then(result => res.json(result))
-    //   .catch(err => {
-    //     console.log(err.message)
-    //     res.status(500).json({result: false})
-    //   })
-
     try {
       let result = await kategori.findAll()
       res.json(result)
@@ -20,14 +13,6 @@ const controller = {
 
   show: async (req, res) => {
     const {id} = req.params
-    // kategori.where({id_kategori: id})
-    //   .fetch()
-    //   .then(result => res.json(result))
-    //   .catch(err => {
-    //     console.log(err.message)
-    //     res.status(500).json({result: false})
-    //   })
-
     try {
       let result = await kategori.findById(id)
       res.json(result)
@@ -39,52 +24,29 @@ const controller = {
 
   store: async (req, res) => {
     const data = req.body
-    // new kategori(data)
-    //   .save()
-    //   .then((result) => res.json({result: result.attributes}))
-    //   .catch(err => {
-    //     console.log(err.message)
-    //     res.status(500).json({success: false})
-    //   })
     try {
       let result = await kategori.create(data)
       res.json(result.attributes)
     } catch (err) {
       console.log(err.message)
-      res.status(500).json({result: false})
+      res.status(500).json({err: err.details})
     }
   },
 
   update: async (req, res) => {
     const data = req.body
     const {id} = req.params
-    // new kategori({
-    //   id_kategori: id,
-    //   ...data
-    // })
-    //   .save()
-    //   .then((result) => res.json({result: result.attributes}))
-    //   .catch(err => {
-    //     console.log(err.message)
-    //     res.status(500).json({success: false})
-    //   })
     try {
       let result = await kategori.update(data, {id})
       res.json(result.attributes)
     } catch (err) {
       console.log(err.message)
-      res.status(500).json({result: false})
+      res.status(500).json({err: err.details})
     }
   },
 
   destroy: async (req, res) => {
     const {id} = req.params
-    // new kategori({id_kategori: id})
-    //   .destroy()
-    //   .then(() => res.json({success: true}))
-    //   .catch(() => {
-    //     res.status(500).json({success: false})
-    //   })
     try {
       await kategori.destroy({id})
       res.json({success: true})
--- model/kategori.js
+++ model/kategori.js
@@ -1,9 +1,13 @@
 const modelBase = require('../bookshelf')
+const Joi = require('joi')
 
 const Kategori = modelBase.extend({
   idAttribute: 'id_kategori',
   tableName: 'kategori',
-  hasTimestamps: false
+  hasTimestamps: false,
+  validate: {
+    nama: Joi.string().alphanum()
+  }
 })
 
 module.exports = Kategori
